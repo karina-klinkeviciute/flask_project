407f96263fe3baaa08cca518706042e97bb65994
karina <karina.klinkeviciute@gmail.com>
2024-05-29 18:54:35 +0300

    registration

--- app.py
+++ app.py
@@ -1,13 +1,56 @@
-from flask import Flask, Response, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_migrate import Migrate
+
+from models import db, Product, User
 
 from products import all_products
 
 app: Flask = Flask(__name__)
 
+# database initiation
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+db.init_app(app)
 
-@app.route("/")
-def hello():
-    return 'Hello World!'
+# admin
+admin = Admin(app)
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Product, db.session))
+
+# routes
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user:
+            return render_template('register.html', message='user with this email already exists')
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['email'] = email
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return 'login'
+
+
+@app.route('/logout')
+def logout():
+    return 'logout'
 
 
 @app.route("/contacts/")
@@ -33,5 +76,9 @@ def example(name):
 
 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    migrate = Migrate(app, db)
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.String(80), nullable=True)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    display_name = db.Column(db.String(80))
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    active = db.Column(db.Boolean, nullable=False, default=True)
+
